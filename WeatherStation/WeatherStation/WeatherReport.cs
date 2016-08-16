765ed9ceddc1de089c4d7d1fe7421a74fae12b77
Jeb <jeb.watson@cepedasystems.com>
2016-08-15 22:23:25 -0500

    New Project!
    -Add the WeatherStation C# command-line project
    -Add the WeatherReport Class for representing weather snapshots

---
+++ WeatherStation/WeatherStation/WeatherReport.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherStation
+{
+    class WeatherReport
+    {
+        public int Temperature { get; set; }
+        public int Pressure { get; set; }
+        public int Rainfall { get; set; }
+        public int Altitude { get; set; }
+        public int WindSpeed { get; set; }
+        public int WindDirection { get; set; }
+    }
+}
